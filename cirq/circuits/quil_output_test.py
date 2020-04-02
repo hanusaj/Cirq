871ab643fc3062256347a22ffa51e2637cfa1979
Ubuntu <ubuntu@ip-172-31-17-21.ec2.internal>
2020-04-02 22:59:20 +0000

    Added support for Quil DEFGATE

--- cirq/circuits/quil_output_test.py
+++ cirq/circuits/quil_output_test.py
@@ -12,7 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import re
+import os
+import numpy as np
+import pytest
+
 import cirq
+from cirq.circuits.quil_output import QuilTwoQubitGate, QuilOneQubitGate
+
 
 def _make_qubits(n):
     return [cirq.NamedQubit('q{}'.format(i)) for i in range(n)]
@@ -33,3 +40,38 @@ def test_two_qubit_diagonal_gate():
     q0,q1, = _make_qubits(2)
     output = cirq.QuilOutput((cirq.TwoQubitDiagonalGate([0.34, 0.12, 0, 0.96]),), (q0,q1,))
     print(str(output))
+def test_quil_one_qubit_gate_repr():
+    gate = QuilOneQubitGate(np.array([[1,0],[0,1]]))
+    assert repr(gate) == ("""cirq.circuits.quil_output.QuilOneQubitGate(matrix=
+[[1,0]
+ [0,1]]
+)""")
+
+def test_quil_one_qubit_gate_eq():
+    gate = QuilOneQubitGate(np.array([[1,0],[0,1]]))
+    gate2 = QuilOneQubitGate(np.array([[1,0],[0,1]]))
+    assert(cirq.approx_eq(gate, gate2, atol=1e-16))
+    gate3 = QuilOneQubitGate(np.array([[1,0],[0,1]]))
+    gate4 = QuilOneQubitGate(np.array([[1,0],[0,2]]))
+    assert(not cirq.approx_eq(gate4, gate3, atol=1e-16))
+
+
+def test_quil_two_qubit_gate_repr():
+    cirq.testing.assert_equivalent_repr(QuilTwoQubitGate(
+        cirq.testing.random_unitary(4)))
+
+
+def test_quil_two_qubit_gate_unitary():
+    u = cirq.testing.random_unitary(2)
+    g = QuilOneQubitGate(u)
+    cirq.testing.assert_allclose_up_to_global_phase(cirq.unitary(g),
+                                                    u,
+                                                    atol=1e-7)
+
+    cirq.testing.assert_implements_consistent_protocols(g)
+
+
+def test_quil_two_qubit_gate_unitary():
+    u = cirq.testing.random_unitary(4)
+    g = QuilTwoQubitGate.from_matrix(u)
+    np.testing.assert_allclose(cirq.unitary(g), u)
